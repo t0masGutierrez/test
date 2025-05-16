54bf8a089a6a2294dd914de5bc5cd9f36ccc0094
Tomas Gutierrez <89363759+t0masGutierrez@users.noreply.github.com>
2025-05-16 14:59:49 -0500

    datetime and plot (#1)
    * updated datetime
    
    * plotted cosine function

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-x = np.arange(9).reshape(3,3)
-print(x)
+x = np.linspace(-100, 100, 1000)
+y = np.cos(x)
+plt.plot(x, y)
+plt.xlim((-2*np.pi, 2*np.pi))
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("y = cos(x)")
+plt.show()
